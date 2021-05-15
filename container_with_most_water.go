832245618c5f680af7ba5205084991e4686a54d0
Hui Zhang <hui.zhang@cootek.cn>
2021-05-15 08:22:56 +0800

    init, container-with-most-water

---
+++ container_with_most_water.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+/*
+https://leetcode-cn.com/problems/container-with-most-water/submissions/
+*/
+
+func maxArea(height []int) int {
+	beginIndex, endIndex, res := 0, len(height)-1, 0
+	for beginIndex <= endIndex {
+		begin, end := height[beginIndex], height[endIndex]
+		curRes := 0
+		if begin > end {
+			curRes = end * (endIndex - beginIndex)
+			endIndex--
+		} else {
+			curRes = begin * (endIndex - beginIndex)
+			beginIndex++
+		}
+		if res < curRes {
+			res = curRes
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Print(maxArea([]int{1,8,6,2,5,4,8,3,7}))
+}
